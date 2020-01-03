981a6087fcccb2ea1d4e40ef56ea1b6ef435126a
jianyli <172961950@qq.com>
2020-01-03 23:07:42 +0800

    start

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import {getRequest} from './utils/api'
+import {postRequest} from './utils/api'
+import {deleteRequest} from './utils/api'
+import {putRequest} from './utils/api'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.isNotNullORBlank = isNotNullORBlank;
 
 new Vue({
   render: h => h(App),
